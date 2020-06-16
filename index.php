6fc05aae9ed76de301282b86fd575da39a420f8b
calanis99 <66448977+calanis99@users.noreply.github.com>
2020-06-16 18:25:59 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form method="POST" action="operacion.php">
+	<label>ingrese el primer numero</label><br>
+	<input type="number" name="numero1"><br>
+
+	<label>ingrese el segundo numero</label><br>
+	<input type="number" name="numero2"><br>
+
+	<button type="submit">ver resultado</button>
+</form>
+</body>
+</html>
---
+++ operaciones.php
@@ -0,0 +1,7 @@
+<?php 
+	$num1=$_POST['numero1']
+	$num2=$_POST['numero2']
+
+	$suma=$num1+$num2;
+	echo "el resultado es : ".$suma;
+ ?>
